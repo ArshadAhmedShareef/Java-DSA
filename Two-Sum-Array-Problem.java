0880e47e0be10815bdfc2d80fc04bede9a29623b
Arshad Ahmed Shareef <76745534+ArshadAhmedShareef@users.noreply.github.com>
2023-10-17 20:48:44 +0530

    Update Two-Sum-Array-Problem.java

--- Two-Sum-Array-Problem.java
+++ Two-Sum-Array-Problem.java
@@ -6,6 +6,7 @@ public class main{
     int nums= {2,7,11,15};
     int target=9;
     toBruteForce(nums,target); //function call
+	hashMap(nums,target); 
     
   }
   public static int[] toBruteForce(int[] nums,int target){  //return type is array so int[] is there instead of void
@@ -26,5 +27,22 @@ public class main{
         
     
   }
+
+	public int[] hashMap(int[] nums, int target) {
+			Map<Integer,Integer> numsMap =new HashMap<>();
+
+			for(int i=0;i<nums.length;i++){
+			int	numstoFind=target-nums[i];
+				 
+				 
+				 if(numsMap.containsKey(numstoFind)){
+					 return new int[] {numsMap.get(numstoFind),i};
+				 }
+				 numsMap.put(nums[i],i);//putting the values in the hash table 
+
+			}
+
+			return new int[] {};    
+    }
 
 }
